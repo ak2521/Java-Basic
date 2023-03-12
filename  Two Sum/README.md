3630b2f9805b28fad367e9ebfe9ee01cd4486ca2
Ajay Kumar T <akvnr2152002@gmail.com>
2023-03-12 17:35:35 +0530

    LeetCode

---
+++  Two Sum/README.md
@@ -0,0 +1,1 @@
+<img width="1440" alt="Result" src="https://user-images.githubusercontent.com/105222251/224471573-85f7aac2-d3a2-466c-b3ce-95ddca58b623.png">
---
+++  Two Sum/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int sum=0;
+        int len=nums.length;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=i+1;j<len;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+    
+}
